23a0b575ce556fa07db63d04a5c635d9356aa9f6
KING-258 <amulyaparashar258@gmail.com>
2024-04-16 10:08:49 +0530

    JS Project - II

--- KnapSack_FrontEND/src/App.js
+++ KnapSack_FrontEND/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 function App() {
-  const [numItems, setNumItems] = useState(0);
+  const [n, setN] = useState(0);
   const [items, setItems] = useState([]);
-  const [capacity, setCapacity] = useState(0);
-  const [knapsackType, setKnapsackType] = useState(0);
-  const [maxValue, setMaxValue] = useState(0);
+  const [c, setC] = useState(0);
+  const [kType, setKType] = useState(0);
+  const [maxV, setMaxV] = useState(0);
   const [selectedItems, setSelectedItems] = useState([]);
 
-  const handleCalculate = () => {
+  const calculate = () => {
     const requestData = {
-      num_items: numItems,
+      num_items: n,
       items: items,
-      capacity: capacity,
-      knapsack_type: knapsackType
+      capacity: c,
+      knapsack_type: kType
     };
 
     fetch('http://localhost:5000/calculate', {
@@ -25,7 +26,7 @@ function App() {
     })
     .then(response => response.json())
     .then(data => {
-      setMaxValue(data.max_value);
+      setMaxV(data.max_value);
       setSelectedItems(data.selected_items);
     })
     .catch(error => {
@@ -33,12 +34,12 @@ function App() {
     });
   };
 
-  const handleClear = () => {
-    setNumItems(0);
+  const clear = () => {
+    setN(0);
     setItems([]);
-    setCapacity(0);
-    setKnapsackType(0);
-    setMaxValue(0);
+    setC(0);
+    setKType(0);
+    setMaxV(0);
     setSelectedItems([]);
   };
 
@@ -47,33 +48,33 @@ function App() {
       <h1>Knapsack Problem Solver</h1>
       <div className="input-container">
         <label>Number of Items: </label>
-        <input type="number" value={numItems} onChange={e => setNumItems(parseInt(e.target.value))} />
+        <input type="number" value={n} onChange={e => setN(parseInt(e.target.value))} />
       </div>
-      <ItemsInput numItems={numItems} setItems={setItems} />
+      <ItemsInput numItems={n} setItems={setItems} />
       <div className="input-container">
         <label>Capacity of Knapsack: </label>
-        <input type="number" value={capacity} onChange={e => setCapacity(parseInt(e.target.value))} />
+        <input type="number" value={c} onChange={e => setC(parseInt(e.target.value))} />
       </div>
       <div className="input-container">
         <label>Knapsack Type: </label>
-        <select value={knapsackType} onChange={e => setKnapsackType(parseInt(e.target.value))}>
+        <select value={kType} onChange={e => setKType(parseInt(e.target.value))}>
           <option value={0}>0/1 Knapsack</option>
           <option value={1}>Fractional Knapsack</option>
         </select>
       </div>
       <div className="button-container">
-        <button onClick={handleCalculate}>Calculate</button>
-        <button onClick={handleClear}>Clear Result</button>
+        <button onClick={calculate}>Calculate</button>
+        <button onClick={clear}>Clear Result</button>
       </div>
       <div className="result-container">
         <h2>Results:</h2>
-        <p>Maximum Value: {knapsackType === 1 ? maxValue.toFixed(2) : maxValue}</p>
+        <p>Maximum Value: {kType === 1 ? maxV.toFixed(2) : maxV}</p>
         <p>Selected Items:</p>
         {selectedItems.map((item, index) => (
           <div key={index}>
             <p className="item-info">Item Weight: {item[0]}</p>
             <p className="item-info">Item Profit: {item[1]}</p>
-            {knapsackType === 1 && <p className="item-info">Portion: {item[2]}</p>}
+            {kType === 1 && <p className="item-info">Portion: {item[2]}</p>}
           </div>
         ))}
       </div>
@@ -113,7 +114,7 @@ function ItemsInput({ numItems, setItems }) {
     return inputs;
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     const newItems = [];
     for (let i = 0; i < numItems; i++) {
       newItems.push([parseFloat(weights[i]) || 0, parseFloat(profits[i]) || 0]);
