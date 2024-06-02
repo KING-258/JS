78dbbee0831961681584d9e1b01c83283fce822f
KING-258 <amulyaparashar258@gmail.com>
2024-06-03 00:24:08 +0530

    JS Project - XI

---
+++ Music_Player/config-override.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    fs: false,
+    buffer: require.resolve('buffer/')
+  };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    })
+  ]);
+
+  return config;
+};
